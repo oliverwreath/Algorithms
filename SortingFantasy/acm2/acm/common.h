52db477dbe2632f7f439d8702f3416948b92317a
oliverwreath <skywalkerhunter@gmail.com>
2013-11-17 11:55:34 +0800

    3/2 comparisons for Selection-sort
    with the switch to less overhead version when array gets small.

--- SortingFantasy/acm2/acm/common.h
+++
@@ -1,9 +0,0 @@
-
-
-
-
-#define F(i, a, b) for(int i = a; i <= b; i++)
-#define F2(i, a, b) for(int i = a; i <= b; i+=2)
-#define FB(i, a, b) for(int i = a; i >= b; i--)
-
-#define MAX3(a, b, c) (a>b?a:b)>c?(a>b?a:b):c
---
+++ SortingFantasy/sorting.h
@@ -0,0 +1,31 @@
+
+
+#include <common.h>
+//
+int merge( int in_array [], int m, int begin, int end );
+
+void mergeSort( int in_array [], int begin, int end );
+
+//
+int partition( int A [], int p, int r );
+
+void quickSort( int A [], int p, int r );
+
+//
+int randPartition( int A [], int p, int r );
+
+void randQuickSort( int A [], int p, int r );
+
+//
+void insertionSort( int A [], int p, int r );
+
+void selectionSort( int A [], int p, int r );
+
+void selectionSort2Way( int A [], int p, int r );
+
+void selectionSort2WayV2( int A [], int p, int r );
+
+void bbSort( int A [], int p, int r );
+
+//
+bool doneYet( int A [], int p, int r );
